30083c38f1ef085ee9d390ec880b2fde9a2d024f
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-07-14 18:30:11 +0300

    Assignment Done

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+// function Tickler() {
+// return <button onClick={()=>console.log("Teehee!")}>Tickle me!</button>
+// }
 function Tickler() {
   function tickle() {
     console.log("Teehee!");
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
